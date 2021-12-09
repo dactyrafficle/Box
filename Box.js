fee904ba2474c837700b4e1c60353dd7a6f664b9
dactyrafficle <40529986+dactyrafficle@users.noreply.github.com>
2021-12-09 06:51:54 -0500

    Add files via upload

--- Box.js
+++ Box.js
@@ -80,13 +80,27 @@ Box.prototype.returnCanvas = function() {
  return this.c;
 }
 
-
+Box.prototype.RETURN_CANVAS = function() {
+ this.c.addEventListener('click', function(e) {
+  let pixel = {'x':e.offsetX,'y':e.offsetY};
+  let val = this.PIXEL2VAL(pixel);
+  //this.recenter(val);
+ }.bind(this)); // makes this.c inherit 'this' from Box
+ return this.c;
+}
 
 Box.prototype.border = function(x) {
  this.c.style.border = x;
 }
 
 Box.prototype.dimension = function(w, h) {
+ this.data.dimension.w = w;
+ this.data.dimension.h = h; 
+ this.c.width = this.data.dimension.w;
+ this.c.height = this.data.dimension.h;
+}
+
+Box.prototype.CANVAS_SIZE = function(w, h) {
  this.data.dimension.w = w;
  this.data.dimension.h = h; 
  this.c.width = this.data.dimension.w;
@@ -111,7 +125,6 @@ Box.prototype.rangex = function(min, max) {
  this.data.zoom.x = this.data.dimension.w / this.data.range.x.span;
  this.data.translate.x = -this.data.range.x.min;
 }
-
 Box.prototype.rangey = function(min, max) {
  this.data.range.y.min = min;
  this.data.range.y.max = max;
@@ -121,9 +134,31 @@ Box.prototype.rangey = function(min, max) {
  this.data.translate.y = -this.data.range.y.min;
 }
 
+Box.prototype.RANGE_X = function(min, max) {
+ this.data.range.x.min = min;
+ this.data.range.x.max = max;
+ this.data.range.x.span = max-min;
+
+ this.data.zoom.x = this.data.dimension.w / this.data.range.x.span;
+ this.data.translate.x = -this.data.range.x.min;
+}
+Box.prototype.RANGE_Y = function(min, max) {
+ this.data.range.y.min = min;
+ this.data.range.y.max = max;
+ this.data.range.y.span = max-min;
+
+ this.data.zoom.y = this.data.dimension.h / this.data.range.y.span;
+ this.data.translate.y = -this.data.range.y.min;
+}
 
 
 Box.prototype.CLEAR_CANVAS = function() {
+ this.ctx.fillStyle = '#fff';
+ this.ctx.beginPath();
+ this.ctx.rect(0, 0, this.data.dimension.w, this.data.dimension.h);
+ this.ctx.fill();
+}
+Box.prototype.clear = function() {
  this.ctx.fillStyle = '#fff';
  this.ctx.beginPath();
  this.ctx.rect(0, 0, this.data.dimension.w, this.data.dimension.h);
@@ -172,6 +207,7 @@ Box.prototype.SHOWGRIDY = function(dy) {
 
 };
 
+
 Box.prototype.SHOW_GRID_X = function(dx, color_string) {
 
  if (!arguments[0]) {
@@ -200,6 +236,8 @@ Box.prototype.SHOW_GRID_X = function(dx, color_string) {
   i++;
  }
 };
+
+
 Box.prototype.SHOW_GRID_Y = function(dy, color_string) {
 
  if (!arguments[0]) {
@@ -602,6 +640,284 @@ function abc(x, arr) {
  }
  
 }
+Box.prototype.DRAW_HISTOGRAM = function(obj) {
+  
+  // obj.data is what i need
+  let n_bins = obj.k;
+  let bin_width = this.data.range.x.span / n_bins;
+  console.log(obj);
+  for (let i = 0; i < n_bins; i++) {
+    //console.log('this');
+    this.RECT_OUTLINE({'x':i*bin_width,'y':0}, bin_width, -obj.data[i].count, '#aaa', 1); 
+  }
+  
+}
+
+
+
+
+
+/*
+  let obj = {
+    'val':val,
+    'color_string':'#000',
+    'rx':1
+  }
+*/
+Box.prototype.DRAW_VALUE = function(obj) {
+ let pixel = this.VAL2PIXEL(obj.val);
+ this.ctx.fillStyle = obj.colorstring;
+ this.ctx.beginPath();
+ this.ctx.arc(pixel.x, pixel.y, obj.rx, 0, 2*Math.PI);
+ this.ctx.fill();
+}
+
+
+
+
+
+/*
+  obj = {
+    'alpha':[alpha0, alpha1],
+    'budget':[budget0, budget1],
+    'px':[px0, px1],
+    'py':[py0, py1],
+    'marshallian':true,
+    'hicksian':true,
+    'log':false,
+    'color_string':'#000',
+    'line_width':2
+    'rx':4
+  }
+*/
+
+Box.prototype.DRAW_DEMAND_CURVE = function(obj) {
+
+  let line_width = 2;
+  let rx = 4;
+  
+  let colors = {
+    'marshallian':{
+      'initial':{
+        'x':'#ffe6b3', // CASE 0
+        'y':'#ffe6b3'  // CASE 1
+      },
+      'final':{
+        'x':'#d1e0e0',  // CASE 2
+        'y':'#d1e0e0'   // CASE 3
+      }
+    },
+    'hicksian':{
+      'initial':{
+        'x':'#f937',  // CASE 4 : INITIAL HICKSIAN X
+        'y':'#ffc266'   // CASE 5 : INITIAL HICKSIAN Y
+      },
+      'final':{
+        'x':'#c2d1f0',  // CASE 6 : FINAL HICKSIAN X
+        'y':'#c2d1f0'   // CASE 7
+      }
+    }
+  }
+
+  // LOCAL VARIABLES
+
+  let alpha_0, alpha_1;
+  let beta_0, beta_1;
+  let budget_0, budget_1;
+  let px_0, px_1;
+  let py_0, py_1;
+
+  alpha_0 = (obj.alpha) ? (obj.alpha[0]) : (null);
+  alpha_1 = (obj.alpha) ? (obj.alpha[1]) : (null);
+  
+  beta_0 = (obj.beta) ? (obj.beta[0]) : (null);
+  beta_1 = (obj.beta) ? (obj.beta[1]) : (null);
+  
+  budget_0 = (obj.budget) ? (obj.budget[0]) : (null);
+  budget_1 = (obj.budget) ? (obj.budget[1]) : (null);
+
+  px_0 = (obj.px) ? (obj.px[0]) : (null);
+  px_1 = (obj.px) ? (obj.px[1]) : (null);
+  
+  py_0 = (obj.py) ? (obj.py[0]) : (null);
+  py_1 = (obj.py) ? (obj.py[1]) : (null);
+
+  // THE RESULTING ALLOCATIONS
+
+  let x_0 = alpha_0 * budget_0 / px_0;
+  let y_0 = beta_0 * budget_0 / py_0;
+  let u_0 = x_0**alpha_0*y_0**beta_0;
+
+  let x_1 = alpha_1 * budget_1 / px_1;
+  let y_1 = beta_1 * budget_1 / py_1;
+  let u_1 = x_1**alpha_1*y_1**beta_1;
+  
+  let xc_0 = (u_0*(py_0/px_0*alpha_0/beta_0)**beta_0);
+  let yc_0 = (u_0*(px_0/py_0*beta_0/alpha_0)**alpha_0);
+  
+  let xc_1 = (u_0*(py_1/px_1*alpha_1/beta_1)**beta_1);
+  let yc_1 = (u_0*(px_1/py_1*beta_1/alpha_1)**alpha_1);
+
+
+  // PREP FOR LOOP
+
+  let dx = this.data.range.x.span/50;
+  let temp = new Array(8);
+  temp[0] = {'x':0,'y':alpha_0 * budget_0 / (dx/1000)};   // INITIAL MARSHALLIAN X
+  temp[1] = {'x':0,'y':beta_0 * budget_0 / (dx/1000)};    // INITIAL MARSHALLIAN Y
+  
+  temp[2] = {'x':0,'y':alpha_1 * budget_1 / (dx/1000)};   // INITIAL HICKSIAN X
+  temp[3] = {'x':0,'y':beta_1 * budget_1 / (dx/1000)};    // INITIAL HICKSIAN Y
+ 
+  temp[4] = {'x':0,'y':u_0*(py_0/(dx/1000)*alpha_0/beta_0)**beta_0}; // MARSHALLIAN RESPONSE X
+  temp[5] = {'x':0,'y':u_0*(px_0/(dx/1000)*beta_0/alpha_0)**alpha_0}; // MARSHALLIAN RESPONSE Y
+  
+  temp[6] = {'x':0,'y':u_0*(py_1/(dx/1000)*alpha_1/beta_1)**beta_1}; // HICKSIAN RESPONSE X
+  temp[7] = {'x':0,'y':u_0*(px_1/(dx/1000)*beta_1/alpha_1)**alpha_1}; // HICKSIAN RESPONSE Y
+
+  let initial_marshallian_x = (obj.x[0] && obj.marshallian) ? (true) : (false);
+  let initial_marshallian_y = (obj.y[0] && obj.marshallian) ? (true) : (false);
+  let final_marshallian_x = (obj.x[1] && obj.marshallian) ? (true) : (false);
+  let final_marshallian_y = (obj.y[1] && obj.marshallian) ? (true) : (false);
+  
+  let initial_hicksian_x = (obj.x[0] && obj.hicksian) ? (true) : (false);
+  let initial_hicksian_y = (obj.y[0] && obj.hicksian) ? (true) : (false);
+  let final_hicksian_x = (obj.x[1] && obj.hicksian) ? (true) : (false);
+  let final_hicksian_y = (obj.y[1] && obj.hicksian) ? (true) : (false);
+  
+  // DRAW IN LOOP
+  
+  for (let x = dx; x < this.data.range.x.max+dx; x += dx) {
+
+    // CASE 6 : FINAL HICKSIAN X
+    if (obj.x[1] && obj.hicksian) {
+      let y = u_0*(py_1/x*alpha_1/beta_1)**beta_1;
+      let val = {'x':x,'y':y};
+      this.CONNECTVALUES(temp[6], val, colors.hicksian.final.x, line_width);
+      temp[6] = val;
+      
+      let pixel = this.VAL2PIXEL({'x':px_1,'y':xc_1});
+      this.ctx.fillStyle = colors.hicksian.final.x;
+      this.ctx.beginPath();
+      this.ctx.arc(pixel.x, pixel.y, rx, 0, 2*Math.PI);
+      this.ctx.fill();
+    }
+    
+    // CASE 7 : FINAL HICKSIAN Y
+    if (obj.y[1] && obj.hicksian) {
+      let y = u_0*(px_1/x*beta_1/alpha_1)**alpha_1;
+      let val = {'x':x,'y':y};
+      this.CONNECTVALUES(temp[7], val, colors.hicksian.final.y, line_width);
+      temp[7] = val;
+      
+      let pixel = this.VAL2PIXEL({'x':py_1,'y':yc_1});
+      this.ctx.fillStyle = colors.hicksian.final.y;
+      this.ctx.beginPath();
+      this.ctx.arc(pixel.x, pixel.y, rx, 0, 2*Math.PI);
+      this.ctx.fill();
+    }
+
+
+    // CASE 4
+    if (initial_hicksian_x) {
+      let y = u_0*(py_0/x*alpha_0/beta_0)**beta_0;
+      let val = {'x':x,'y':y};
+      this.CONNECTVALUES(temp[4], val, colors.hicksian.initial.x, line_width);
+      temp[4] = val;
+      
+      let pixel = this.VAL2PIXEL({'x':px_0,'y':u_0*(py_0/px_0*alpha_0/beta_0)**beta_0});
+      this.ctx.fillStyle = colors.hicksian.initial.x;
+      this.ctx.beginPath();
+      this.ctx.arc(pixel.x, pixel.y, rx, 0, 2*Math.PI); // THIS POINT WILL ACTUALLY NOT BE SEEN
+      this.ctx.fill();
+    }
+
+    // CASE 5
+    if (initial_hicksian_y) {
+      let y = u_0*(px_0/x*beta_0/alpha_0)**alpha_0;
+      let val = {'x':x,'y':y};
+      this.CONNECTVALUES(temp[5], val, colors.hicksian.initial.y, line_width);
+      temp[5] = val;
+      
+      let pixel = this.VAL2PIXEL({'x':py_0,'y':u_0*(px_0/py_0*beta_0/alpha_0)**alpha_0});
+      this.ctx.fillStyle = colors.hicksian.initial.y;
+      this.ctx.beginPath();
+      this.ctx.arc(pixel.x, pixel.y, rx, 0, 2*Math.PI); // THIS POINT WILL ACTUALLY NOT BE SEEN
+      this.ctx.fill();
+    }
+
+    // CASE 3 : FINAL MARSHALLIAN Y
+    if (final_marshallian_y) {
+      let y = beta_1 * budget_1 / x;
+      let val = {'x':x,'y':y};
+      this.CONNECTVALUES(temp[3], val, colors.marshallian.final.y, line_width);
+      temp[3] = val;
+      
+      let pixel = this.VAL2PIXEL({'x':py_1,'y':(beta_1 * budget_1 / py_1)});
+      this.ctx.fillStyle = colors.marshallian.final.y;
+      this.ctx.beginPath();
+      this.ctx.arc(pixel.x, pixel.y, rx, 0, 2*Math.PI);
+      this.ctx.fill();
+    }
+
+    // CASE 2 : FINAL MARSHALLIAN X
+    if (final_marshallian_x) {
+      let y = alpha_1 * budget_1 / x;
+      let val = {'x':x,'y':y};
+      this.CONNECTVALUES(temp[2], val, colors.marshallian.final.x, line_width);
+      temp[2] = val;
+      
+      let pixel = this.VAL2PIXEL({'x':px_1,'y':(alpha_1 * budget_1 / px_1)});
+      this.ctx.fillStyle = colors.marshallian.final.x;
+      this.ctx.beginPath();
+      this.ctx.arc(pixel.x, pixel.y, rx, 0, 2*Math.PI);
+      this.ctx.fill();
+    }
+    
+    // CASE 1 : INITIAL MARSHALLIAN Y
+    if (initial_marshallian_y) {
+      let y = beta_0 * budget_0 / x;
+      let val = {'x':x,'y':y};
+      this.CONNECTVALUES(temp[1], val, colors.marshallian.initial.y, line_width);
+      temp[1] = val;
+      
+      let pixel = this.VAL2PIXEL({'x':py_0,'y':(beta_0 * budget_0 / py_0)});
+      this.ctx.fillStyle = colors.marshallian.initial.y;
+      this.ctx.beginPath();
+      this.ctx.arc(pixel.x, pixel.y, rx, 0, 2*Math.PI);
+      this.ctx.fill();
+    }
+    
+    // CASE 0 : INITIAL MARSHALLIAN X
+    if (initial_marshallian_x) {
+      let y = alpha_0 * budget_0 / x;
+      let val = {'x':x,'y':y};
+      this.CONNECTVALUES(temp[0], val, colors.marshallian.initial.x, line_width);
+      temp[0] = val;
+      
+      let pixel = this.VAL2PIXEL({'x':px_0,'y':(alpha_0 * budget_0 / px_0)});
+      this.ctx.fillStyle = colors.marshallian.initial.x;
+      this.ctx.beginPath();
+      this.ctx.arc(pixel.x, pixel.y, rx, 0, 2*Math.PI);
+      this.ctx.fill();
+    }
+
+  } // closing the loop
+
+  return {
+    'initial':{
+      'u':u_0,
+      'marshallian':{'x':x_0,'y':y_0},
+      'hicksian':{'x':xc_0,'y':yc_0}
+    },
+    'final':{
+      'u':u_1,
+      'marshallian':{'x':x_1,'y':y_1},
+      'hicksian':{'x':xc_1,'y':yc_1}
+    }
+  };
+  
+}
 
 
 /*
@@ -616,7 +932,12 @@ function abc(x, arr) {
   'py':py,
   'color_string':#000,
   'line_width':1,
-  'inverted':false
+  'inverted':false,
+  'allocation':{
+    'show':true,
+    'color_string':#000,
+    'rx':3
+  }
  }
 */
 Box.prototype.DRAW_ISOQUANT = function(obj) {
@@ -628,7 +949,9 @@ Box.prototype.DRAW_ISOQUANT = function(obj) {
   // case 2 : u=f(x,y)
   // case 3 : x=f(M,px) : y=f(M,py) : u=f(x,y)
   
-  let alpha, alpha_inv, beta, beta_inv, u;
+  let alpha, alpha_inv, beta, beta_inv;
+  let u;
+  let x, y;
 
   if (!obj.alpha || !obj.beta) {
     console.log('you need alpha and beta');
@@ -647,8 +970,8 @@ Box.prototype.DRAW_ISOQUANT = function(obj) {
 
   // SECOND CASE : u = u(x, y)
   if (!obj.u && (obj.x && obj.y)) {
-    let x = obj.x;
-    let y = obj.y;
+    x = obj.x;
+    y = obj.y;
     u = x**alpha*y**beta;
   }
 
@@ -658,11 +981,10 @@ Box.prototype.DRAW_ISOQUANT = function(obj) {
     let M = obj.M;
     let px = obj.px;
     let py = obj.py;
-    let x = alpha*M/px;
-    let y = beta*M/py;
+    x = alpha*M/px;
+    y = beta*M/py;
     u = x**alpha*y**beta;
   }
-  
   
   let string_color;
   let line_width;
@@ -701,47 +1023,122 @@ Box.prototype.DRAW_ISOQUANT = function(obj) {
     } 
   }
 
-
-
-
+  // SHOW ALLOCATION
+  if (obj.allocation.show) {
+    let pixel = this.VAL2PIXEL({'x':x,'y':y});
+    this.ctx.fillStyle = (obj.allocation.colorstring || '#fc0');
+    this.ctx.beginPath();
+    this.ctx.arc(pixel.x, pixel.y, (obj.allocation.rx || 3), 0, 2*Math.PI);
+    this.ctx.fill();
+  }
+  
 }
 
 
-Box.prototype.DRAW_HISTOGRAM = function(obj) {
-  
-  // obj.data is what i need
-  let n_bins = obj.k;
-  let bin_width = this.data.range.x.span / n_bins;
+
+/*
+let obj = {
+  'points':[
+    {'x':0,'y':0},
+    {'x':0,'y':0},
+    {'x':0,'y':0}
+  ]
+}
+*/
+Box.prototype.RESCALE_BASED_ON_CM = function(obj) {
+
   console.log(obj);
-  for (let i = 0; i < n_bins; i++) {
-    //console.log('this');
-    this.RECT_OUTLINE({'x':i*bin_width,'y':0}, bin_width, -obj.data[i].count, '#aaa', 1); 
+
+  let cx = 0;
+  let cy = 0;
+  
+  let x_min = +(9**5);
+  let x_max = -(9**5);
+  let y_min = +(9**5);
+  let y_max = -(9**5);
+
+  for (let i = 0; i < obj.points.length; i++) {
+    cx += obj.points[i].x;
+    cy += obj.points[i].y; 
+    
+    if (obj.points[i].x > x_max) {
+      x_max = obj.points[i].x;
+    }
+    if (obj.points[i].x < x_min) {
+      x_min = obj.points[i].x;
+    }
+    if (obj.points[i].y > y_max) {
+      y_max = obj.points[i].y;
+    }
+    if (obj.points[i].y < y_min) {
+      y_min = obj.points[i].y;
+    }
+
   }
 
-  
+  let dx = x_max - x_min;
+  let dy = y_max - y_min;
+
+  this.RANGE_X(0, x_max*5/4);
+  this.RANGE_Y(0, y_max*5/4);
+
+  return {
+    'cx':cx,
+    'cy':cy,
+    'dx':dx,
+    'dy':dy,
+    'x_min':x_min,
+    'x_max':x_max,
+    'y_min':y_min,
+    'y_max':y_max
+  }
+
 }
 
-Box.prototype.RESCALE_BASED_ON_CM = function(alpha, x_scale_min, y_scale_min, VERTICES) {
+/*
+let obj = {
+  'points':[
+    {'x':0,'y':0},
+    {'x':0,'y':0},
+    {'x':0,'y':0}
+  ]
+}
+*/
+Box.prototype.RESCALE_BASED_ON_CENTROID = function(obj) {
 
-  let cx = alpha*VERTICES[0].x + (1-alpha)*VERTICES[1].x;
-  let cy = alpha*VERTICES[0].y + (1-alpha)*VERTICES[1].y;
+  console.log(obj);
+  let a = obj.points[0];
+  let b = obj.points[1];
+  let c = obj.points[2];
+
+  let cx = (a.x + b.x + c.x)/3;
+  let cy = (a.y + b.y + c.y)/3;
   
-  let dx = Math.abs(VERTICES[0].x - VERTICES[1].x);
-  let dy = Math.abs(VERTICES[0].y - VERTICES[1].y);
-  
- // if (VERTICES[0].x == VERTICES[1].x && VERTICES[0].y == VERTICES[1].y) {
-    this.rangex(cx-x_scale_min/2, cx+x_scale_min/2);
-    this.rangey(cy-y_scale_min/2, cy+y_scale_min/2);
-    //return;
-  //}
-  
-  if (dx > 0.25*this.data.range.x.span) {
-    this.rangex(cx-dx*2, cx+dx*2);
+  let ra = ((a.x - cx)**2 + (a.y - cy)**2)**0.5;
+  let rb = ((b.x - cx)**2 + (b.y - cy)**2)**0.5;
+  let rc = ((c.x - cx)**2 + (c.y - cy)**2)**0.5;
+
+  let r = 0;
+  if (ra > rb) {
+    r = ra;
+  } else {
+    r = rb;
   }
-  if (dy > 0.25*this.data.range.y.span) {
-    this.rangey(cy-dy*2, cy+dy*2);
+  if (rc > r) {
+    r = rc;
+  }
+  
+  if (a.x == b.x && b.x == c.x &&  a.y == b.y && b.y == c.y) {
+    r = 0.1;
+  }
+  
+  let z = r*3;
+  if (z < 0.5) {
+    z = 0.5;
   }
 
-
+ // RESCALE
+ this.rangex(cx-z, cx+z);
+ this.rangey(cy-z, cy+z);
   
 }
